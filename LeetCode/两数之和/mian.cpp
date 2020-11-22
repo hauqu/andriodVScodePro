7e61b2018a5b418ba7a78fc840bff887b95b4967
huaqu <18098423927@163.com>
2020-11-22 23:10:53 +0800

    更改

--- LeetCode/两数之和/mian.cpp
+++ LeetCode/两数之和/mian.cpp
@@ -3,19 +3,29 @@ using namespace std;
 int nums[10]={5,7,9,2,4,24,27,26,11,88};
 int target =28;
 int len =10;
-
+bool sum1();//暴力枚举
 int main()
 {
-	for(int i=0;i<len-1;i++)
+	sum1();
+    return 0;
+}
+
+bool sum1()
+{
+    //时间复杂度 N*N，空间复杂度 1
+    bool work=false;;
+    for(int i=0;i<len-1;i++)
     {
-        for(int j=1;j<len;j++)
+        for(int j=i+1;j<len;j++)
         {
 			if(nums[i]+nums[j]==target)
             {
                //cout<<i<<"   "<<j<<endl;
             	cout<<nums[i]<<"+"<<nums[j]<<"="<<target<<endl;
+                work =true;
             }
         }
-    }	
-    return 0;
+    }
+    if(!work) cout<<"没有找到符合的"<<endl;
+    return work;
 }
