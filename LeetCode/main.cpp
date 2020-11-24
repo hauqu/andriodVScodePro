674412fc3122f95a31876b73c290904a6f71f112
huaqu <18098423927@163.com>
2020-11-24 19:19:52 +0800

    代码提示时有时无，换笔记本写

---
+++ LeetCode/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(int arg,char argv[])
+{
+    
+    return 0;
+}
---
+++ LeetCode/numberReverse/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//反转 数字
+// 123 ---321
+int n =12345;
+int main(int arg,char argv[])
+{
+    
+	//string ? string::reverse?
+	string sn;
+    
+    return 0;
+}
