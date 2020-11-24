5744aa19b1c7f629cec71e03b0ff97d32658b386
huaqu <18098423927@163.com>
2020-11-24 20:00:32 +0800

    使用字符串进行反转

--- LeetCode/numberReverse/main.cpp
+++ LeetCode/numberReverse/main.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
 //反转 数字
 // 123 ---321
-int n =12345;
+int n =12356;
 int main(int arg,char argv[])
 {
-    
+    cout<<n<<endl<<"反转后"<<endl;
 	//string ? string::reverse?
 	string sn =std::to_string(n);
-	sn.reserve();
-	n = std::atoi(sn);
-
-    
+	reverse(sn.begin(),sn.end());
+	n =std::stoi(sn);
+	cout<<n<<endl;
     return 0;
 }
