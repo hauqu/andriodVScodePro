32f051ef5599a8392703b129c991f93fa2318876
huaqu <18098423927@163.com>
2020-11-26 08:25:50 +0800

    滑动窗口

--- LeetCode/sonString/main.cpp
+++ LeetCode/sonString/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #include<string>
+#include<set>
+
+#include<unordered_set>
+//该头文件包含一种无序集合
 using namespace std;
 /*
 找出不含重复字符的最长子串
@@ -8,11 +12,19 @@ bool findC(string s, char c);
 string m ="abca";
 
 string sonString(string s);//暴力解法
+/*
+滑动窗口法，查找重复字符串使用哈希表
 
+
+*/
+std::unordered_set<char>occ;
 int main(int arg,char *argv[])
 {
     cout<<m<<endl;
     cout<<sonString(m)<<endl;
+
+
+    
     return 0;
 }
 bool findC(string s, char c)
