cbc693b80c65291f421a79a039a2ff3125c664c8
18098423927@163.com <18098423927@163.com>
2021-01-08 12:16:40 +0800

    测试循环链表

--- 数据结构和算法/链表/doubleCircleList.cpp
+++ 数据结构和算法/链表/doubleCircleList.cpp
@@ -1,9 +1,26 @@
+/*
+测试循环双链表
+
+*/
 #include<iostream>
 #include"doubleCircleList.h"
 using namespace std;
 int main(int argc,char*argv[])
 {
     doubleCircleList test;
-    cout<<test.size();
+   
+    for (int i = 0; i < 10; i++)
+    {
+        test.push(i);
+    }
+     cout<<"size:"<<test.size()<<endl;
+    node *temp = test.root;
+    for(int i =0;i<100;i++)
+    {
+        temp =temp->next;
+        cout<<temp->data<<" ";
+        if(i%10==0)
+        cout<<endl;
+    }
     return 0;
 }
